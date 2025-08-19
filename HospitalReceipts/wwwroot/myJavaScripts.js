0afd084cbaa1997344068a5f940c23ab37334109
hrbtth <42539808+hrbtth@users.noreply.github.com>
2025-08-19 21:42:03 +0530

    print receipt added

---
+++ HospitalReceipts/wwwroot/myJavaScripts.js
@@ -0,0 +1,20 @@
+﻿function printDiv(divId) {
+    var content = document.getElementById(divId).innerHTML;
+    var myWindow = window.open('', '', 'width=800,height=600');
+
+    myWindow.document.write('<html><head><title>Print</title>');
+
+    // bring in your app’s CSS so print looks same as screen
+    document.querySelectorAll('link[rel="stylesheet"]').forEach(function (css) {
+        myWindow.document.write(css.outerHTML);
+    });
+
+    myWindow.document.write('</head><body>');
+    myWindow.document.write(content);
+    myWindow.document.write('</body></html>');
+
+    myWindow.document.close();
+    myWindow.focus();
+    myWindow.print();
+    myWindow.close();
+}
